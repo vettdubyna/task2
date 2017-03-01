1df7a5b777d4034378cb973950b14eb80dc16e76
Devis <dubyna.vitaliy@gmail.com>
2017-03-01 18:12:45 +0200

    Added parsing functions. Need to fix some bugs

--- main.py
+++ main.py
@@ -2,17 +2,24 @@
 
 import sys
 import os
-import time
+import io
+import ConfigParser
+
+from menu_functions.prepare import *
 from scripts.configure_vagrant import *
 
 
-def print_headers(str):
-    print " ====================== " + str + " ====================== "
-
 if __name__ == "__main__":
     try:
+        ec2_params = {}
+        ec2_params = set_params("config/configuration.ini")
         print_headers("Configuring virtualization")
-        ensure_env()
+        #ensure_env()
+        print_headers("Define IP-address")
+        if define_ip():
+            print "IP-address has been changed"
+        else:
+            print "IP-addess has been setted by default"
 
     except Exception as e:
         print "Error: " + str(e)
---
+++ menu_functions/prepare.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+import ConfigParser
+
+import io
+
+import sys
+
+import re
+
+
+def set_params(path):
+    try:
+        params = {}
+        with open(path) as f:
+            sample_conf = f.read()
+        config = ConfigParser.RawConfigParser(allow_no_value=True)
+        config.readfp(io.BytesIO(sample_conf))
+        for section in config.sections():
+            for options in config.options(section):
+                params[options] = config.get(section, options)
+        return params
+    except Exception as e:
+        print "Error: " + str(e)
+        sys.exit(1)
+
+
+def print_headers(str):
+    print " ====================== " + str + " ====================== "
+
+
+def replace_text_in_file(file_name, old, new, count):
+    try:
+        with open(file_name, 'r') as f:
+            filedata = f.read()
+
+        # Replace the target string
+        filedata = filedata.replace(old, new, count)
+
+        # Write the file out again
+        with open(file_name, 'w') as f:
+            f.write(filedata)
+    except Exception as e:
+        print "Error: " + str(e)
+        sys.exit(1)
+
+
+def define_ip():
+    try:
+        if raw_input("The default ip for host is 172.17.175.175. Would you like to change it? (yes/no): ") == "yes":
+            ip_machine = raw_input("Please enter new IP-address: ")
+            ip_router = ""
+            pat = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+            if not pat.match(ip_machine):
+                return False
+            else:
+                lst = ip_machine.split(".")
+                ip_router = lst[0] + "." + lst[1] + "." + lst[2] + "." + "1"
+
+                #change ip-adresses in Vagrantfiles
+                replace_text_in_file("../source/machine/Vagrantfile", "172.17.175.175", ip_machine, 1)
+                replace_text_in_file("../source/router/Vagrantfile", "172.17.168.1", ip_router, 1)
+
+                #change ip-adresses in bash scripts
+                replace_text_in_file("../scripts/configure_machine.sh", "172.17.168.1", ip_router, 1)
+                replace_text_in_file("../scripts/configure_router.sh", "172.17.168.1", ip_router, 1)
+                replace_text_in_file("../scripts/configure_router.sh", "172.17.175.175", ip_machine, 1)
+                print ip_machine + "\n" + ip_router
+            return True
+        else:
+            return False
+    except Exception as e:
+        print "Error: " + str(e)
+        sys.exit(1)

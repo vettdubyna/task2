91585da3ba81218ed361a761e9696e818f005abc
Devis <dubyna.vitaliy@gmail.com>
2017-03-03 11:57:55 +0200

    Created clearing functions

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ import ConfigParser
 
 from menu_functions.prepare import *
 from scripts.configure_vagrant import *
-from scripts.create_boxes import *
+from scripts.boxes_actions import *
 from aws_actions.create_aws_resource import *
 from aws_actions.get_aws_resource import *
-from aws_actions.clean_aws_resources import *
+from aws_actions.clear_aws_resources import *
 from aws_actions.configure_aws_resource import *
 
-if __name__ == "__main__":
+if False: #__name__ == "__main__":
+    ec2_params = {}
+    ec2_id = ""
+    ec2_ip = ""
+    host_name = "host"
+    router_name = "router"
     try:
-        ec2_params = {}
-        ec2_id = ""
-        ec2_ip = ""
         ec2_params = set_params("config/configuration.ini")
         print_headers("Configuring virtualization")
         ensure_env()
@@ -25,21 +27,39 @@ if __name__ == "__main__":
         if define_ip():
             print "IP-address has been changed"
         else:
-            print "IP-addess has been setted by default"
+            print "IP-address has been set by default"
 
         print_headers("Create EC2 instance")
-        ec2_id = create_ec2_instance(ec2_params)
-        ec2_ip = get_ec2_ip(ec2_id)
-        preparing_instance(ec2_ip, "ubuntu", "keys/" + ec2_params["key_name"] + ".pem")
-        print ec2_ip, ec2_id
-        print_headers("Create boxes")
-        print "Creating router..."
-        create_vagrant_box("router", "source/router/Vagrantfile", "scripts/configure_router.sh")
+        try:
+            ec2_id = create_ec2_instance(ec2_params)
+            ec2_ip = get_ec2_ip(ec2_id)
+            preparing_instance(ec2_ip, "ubuntu", "keys/" + ec2_params["key_name"] + ".pem")
+            print ec2_ip, ec2_id
+        except Exception as e:
+            print "Error: " + str(e)
+            terminate_ec2(ec2_id)
+            sys.exit(1)
 
-        print "Creating machine..."
-        create_vagrant_box("machine", "source/machine/Vagrantfile", "scripts/configure_machine.sh")
+        print_headers("Create boxes")
+        try:
+            print "Creating router..."
+            create_vagrant_box(router_name, "source/router/Vagrantfile", "scripts/configure_router.sh")
+            print "Creating machine..."
+            create_vagrant_box(host_name, "source/machine/Vagrantfile", "scripts/configure_machine.sh")
+        except Exception as e:
+            print "Error: " + str(e)
+            terminate_ec2(ec2_id)
+            clear_box(host_name)
+            clear_box(router_name)
+            sys.exit(1)
 
     except Exception as e:
         print "Error: " + str(e)
         terminate_ec2(ec2_id)
+        clear_box(host_name)
+        clear_box(router_name)
         sys.exit(1)
+
+
+clear_box("machine")
+clear_box("router")
--- scripts/boxes_actions.py
+++ scripts/boxes_actions.py
@@ -3,11 +3,12 @@ import os
 import sys
 import shutil
 
-def create_vagrant_box(machine, vfile, script):
+
+def create_vagrant_box(host, vfile, script):
     try:
         #copy Vagrantfile and script to dest. dir
         cwd = os.getcwd()
-        directory = cwd + "/vagrant/" + machine + "/"
+        directory = cwd + "/vagrant/" + host + "/"
         print directory
         os.system("mkdir -p " + directory)
         shutil.copy2(vfile, directory)
@@ -16,6 +17,19 @@ def create_vagrant_box(machine, vfile, script):
         #run vagrant box
         os.environ['VAGRANT_CWD'] = directory
         os.system("vagrant up --provision")
+    except Exception as e:
+        print "Error: " + str(e)
+        sys.exit(1)
+
+
+def clear_box(host):
+    try:
+        cwd = os.getcwd()
+        directory = cwd + "/vagrant/" + host
+        print directory
+        os.environ['VAGRANT_CWD'] = directory + "/"
+        os.system("vagrant halt; vagrant destroy -f")
+        os.system("rm -rf " + directory)
     except Exception as e:
         print "Error: " + str(e)
         sys.exit(1)
